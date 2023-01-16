7fd4bce02065d627b4b05abf4ddc6bcd25e63449
Nataniel <natuss.m@~gmail.com>
2023-01-16 15:08:13 -0300

    Fim da aula

---
+++ tipos-variaveis/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    
+    public static void main(String[] args) throws Exception {
+        // operdor ternário
+
+        // <expressão condicional> ? <condição caso seja true> : <Condição caso seja false>
+        int a, b;
+        a = 5;
+        b = 6;
+
+        //String result = a == b ? "Iguais" : "Diferentes";
+
+        System.out.println(a == b ? "Iguais" : "Diferentes");
+    }
+}
---
+++ tipos-variaveis/tipos-variaveis/src/TiposVar.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class TiposVar {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = - numero;
+        System.out.println(numero);
+
+    }
+}
