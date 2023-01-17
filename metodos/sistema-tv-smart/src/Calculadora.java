10afacb7a817a4c8686511c55ad3c9bbacdd5452
Nataniel <natuss.m@~gmail.com>
2023-01-17 11:53:33 -0300

    Inclusão de classe teste para documentação

---
+++ metodos/sistema-tv-smart/src/Calculadora.java
@@ -0,0 +1,22 @@
+    /**
+     * Esta classe é um teste de documentação.
+     * 
+     * @author nataniel
+     * @version 1.0.0
+     * @since 17/01/2023
+    */ 
+
+public class Calculadora {
+
+    /**
+     * Executa a adição de dois elementos numéricos reais
+     * 
+     * @param n1 primeiro elemento da soma em formato double
+     * @param n2 segundo elemento da soma em formato double
+     * @return adição dos dois elementos 
+     */
+    public double somar(double n1, double n2){
+        return n1 + n2;
+    }
+
+}
